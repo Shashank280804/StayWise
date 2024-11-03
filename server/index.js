12cf538d0e1d545ede32ea3be8eb74ee3d0ce886
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-11-03 23:54:02 +0530

    Added routes for listing and properties

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const dotenv = require("dotenv").config()
 const cors = require("cors")
 
 const authRoutes=require("./routes/auth.js")
+const listingRoutes= require("./routes/listing.js")
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
 app.use("/auth",authRoutes)
+app.use("/properties",listingRoutes)
 
 
 /*MONGOOSE SETUP */
