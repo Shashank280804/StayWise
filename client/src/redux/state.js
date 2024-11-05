fe106d1fd6dbb72323f0f8dbf501203f5ad76f77
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-11-05 23:39:40 +0530

    Pushed some modifications

--- client/src/redux/state.js
+++ client/src/redux/state.js
@@ -19,10 +19,25 @@ export const userSlice = createSlice({
     },
     setListings: (state, action) => {
       state.listings = action.payload.listings
-    }
+    },
+    setTripList: (state, action) => {
+      state.user.tripList = action.payload
+    },
+    setWishList: (state, action) => {
+      state.user.wishList = action.payload
+    },
+    setPropertyList: (state, action) => {
+
+      state.user.propertyList = action.payload
+    },
+    setReservationList: (state, action) => {
+
+      state.user.reseervationList = action.payload
+    },
+
 
   }
 })
 
-export const { setLogin, setLogout,setListings } = userSlice.actions
+export const { setLogin, setLogout, setListings, setTripList, setWishList, setPropertyList, setReservationList } = userSlice.actions
 export default userSlice.reducer
