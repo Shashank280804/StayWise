14a02c541955211c77f19d875491ad6a757c6abd
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-11-05 23:04:54 +0530

    added code Listing Card

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import "../styles/ListingCard.scss";
-import { ArrowForwardIos, ArrowBackIosNew } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import {
+  ArrowForwardIos,
+  ArrowBackIosNew,
+  Favorite,
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setWishList } from "../redux/state";
 
 const ListingCard = ({
   listingId,
@@ -12,6 +19,10 @@ const ListingCard = ({
   category,
   type,
   price,
+  startDate,
+  endDate,
+  totalPrice,
+  booking,
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -26,8 +37,42 @@ const ListingCard = ({
     setCurrentIndex((prevIndex) => (prevIndex + 1) % listingPhotoPaths.length);
   };
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
+  const wishList = user?.wishList || [];
+  const isLiked = wishList.find((item) => item?._id === listingId);
+
+
+  const patchWishList = async () => {
+    //check here user for error and condition
+    if( user?._id !== creator._id){
+    const response = await fetch(
+      `http://localhost:3001/users/${user?._id}/${listingId}`,
+      {
+        method: "PATCH",
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    dispatch(setWishList(data.wishList));
+    
+    }
+    else{
+      return;
+    }
+  };
+
   return (
-    <div className="listing-card">
+    <div
+      className="listing-card"
+      onClick={() => {
+        navigate(`/properties/${listingId}`);
+      }}
+    >
       <div className="slider-container">
         <div
           className="slider"
@@ -42,6 +87,7 @@ const ListingCard = ({
               <div
                 className="prev-button"
                 onClick={(e) => {
+                  e.stopPropagation();
                   goToPrevSlide(e);
                 }}
               >
@@ -50,6 +96,7 @@ const ListingCard = ({
               <div
                 className="next-button"
                 onClick={(e) => {
+                  e.stopPropagation();
                   goToNextSlide(e);
                 }}
               >
@@ -59,11 +106,43 @@ const ListingCard = ({
           ))}
         </div>
       </div>
-      <h3>{city},{province},{country}</h3>
+      <h3>
+        {city},{province},{country}
+      </h3>
       <p>{category}</p>
-       <p>{type}</p>
-       <p><span>${price}</span> per night</p>
 
+      {!booking ? (
+        <>
+          <p>{type}</p>
+          <p>
+            <span>${price}</span> per night
+          </p>
+        </>
+      ) : (
+        <>
+          {" "}
+          <p>
+            {startDate} - {endDate}
+          </p>
+          <p>
+            <span>${totalPrice}</span> total
+          </p>
+        </>
+      )}
+      <button
+        className="favorite"
+        onClick={(e) => {
+          e.stopPropagation();
+          patchWishList();
+        }}
+        disabled={!user}
+      >
+        {isLiked ? (
+          <Favorite sx={{ color: "red" }} />
+        ) : (
+          <Favorite sx={{ color: "white" }} />
+        )}
+      </button>
     </div>
   );
 };
